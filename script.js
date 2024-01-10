df64ffdb84d57d10e00f24b3e450edca30a1e063
Leeam <lucideds@proton.me>
2024-01-10 19:19:38 +0000

    i made it work nvm

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const modeToggle = document.getElementById("modeToggle");
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (isDarkMode) {
+    // Dark mode is preferred by the user or set as default
+    document.documentElement.setAttribute("data-theme", "dark");
+    modeToggle.checked = true; // Set the checkbox to checked
+} else {
+    // Light mode is preferred by the user
+    document.documentElement.setAttribute("data-theme", "light");
+    modeToggle.checked = false; // Set the checkbox to unchecked
+}
 
 modeToggle.addEventListener("change", () => {
     if (modeToggle.checked) {
